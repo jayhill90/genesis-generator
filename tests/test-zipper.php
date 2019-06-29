9bcf4183d0a55f2606196bdeca55745a1bb31b5c
Jay Hill <jay.hill@wpengine.com>
2019-06-28 23:07:13 -0500

    Tests Branch

--- tests/test-zipper.php
+++ tests/test-zipper.php
@@ -11,10 +11,18 @@
 class SampleTest extends WP_UnitTestCase {
 
 	/**
-	 * A single example test.
+	 * Test replace genesis. This should replace instances of Genesis Sample, 
+	 * genesis_sample and genesis-sample with a transformed array
+	 * @datatProvider data_replace_genesis
 	 */
-	public function test_sample() {
+	public function test_replace_genesis() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
+	}
+
+	public function data_replace_genesis() {
+		return [
+			
+		]
 	}
 }
