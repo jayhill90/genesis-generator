8e6fa4ded8390dd2e46523c4fcb0e58757f5dcbe
Jay Hill <jay.hill@wpengine.com>
2019-06-28 22:54:06 -0500

    More tweaks

--- inc/class-zipper.php
+++ inc/class-zipper.php
@@ -5,30 +5,31 @@ namespace GenesisGenerator;
 class Zipper {
 	protected $zip;
 	private $genesis = 'https://github.com/studiopress/genesis-sample/archive/master.zip';
-	private $tmp     = 'tmp_file.zip';
+	private $tmp = '/tmp/tmp_file.zip';
 
 	function __construct( $slug ) {
 		$zip = new \ZipArchive();
 
-		if ( ! copy( $this->genesis, $this->tmp ) ) {
-			\WP_CLI::error( ' Failed to copy temp zip file', true );
+		if ( !copy($this->genesis, $this->tmp ) ) {
+			return \WP_CLI::error(" Failed to copy temp zip file", true);
 		}
 
 		$result = $zip->open( $this->tmp );
 
 		if ( $result ) {
-			$zip->extractTo( '/tmp' );
+			$zip->extractTo("/tmp");
 			$zip->close();
 			$rename = rename( '/tmp/genesis-sample-master/', '/tmp' . '/' . $slug );
 			if ( $rename ) {
-				\WP_CLI::log( 'Renamed temp directory' );
-			} else {
-				\WP_CLI::error( 'Failed to rename temp directory' );
+				\WP_CLI::log( "Renamed temp directory") ;
 			}
-		} else {
-			\WP_CLI::error( 'Failed to open temp zip file', true );
+			else {
+				return \WP_CLI::error( "Failed to rename temp directory" );
+			}
 		}
-
+		else {
+			return \WP_CLI::error( "Failed to open temp zip file" , true); 
+		}
 	}
 
 
